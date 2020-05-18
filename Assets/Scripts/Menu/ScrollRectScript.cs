848d3f014f5636cc9d8fab8077b392897a7ff671
JuanMora-spg <60519779+JuanMora-spg@users.noreply.github.com>
2020-05-18 18:57:17 -0500

    18/05/2020
    Modificacion de Menu

---
+++ Assets/Scripts/Menu/ScrollRectScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollRectScript : MonoBehaviour {
+    private ScrollRect scrollRect;
+    private bool mouseDown, buttonRight, buttonLeft;
+
+    void Start () {
+        scrollRect = GetComponent<ScrollRect> ();
+    }
+
+    void Update () {
+        if (mouseDown == true) {
+            if (buttonRight == true) {
+                ScrollRight ();
+            } else if (buttonLeft == true) {
+                ScrollLeft ();
+            }
+        }
+
+    }
+
+    public void buttonRightIsPressed () {
+        mouseDown = true;
+        buttonRight = true;
+    }
+
+    public void buttonLeftIsPressed () {
+        mouseDown = true;
+        buttonLeft = true;
+    }
+
+    private void ScrollRight () {
+        if (Input.GetMouseButtonUp (0)) {
+            mouseDown = false;
+            buttonRight = false;
+        } else {
+            scrollRect.horizontalNormalizedPosition -= 0.01f;
+        }
+    }
+
+    private void ScrollLeft () {
+        if (Input.GetMouseButtonUp (0)) {
+            mouseDown = false;
+            buttonLeft = false;
+        } else {
+            scrollRect.horizontalNormalizedPosition += 0.01f;
+        }
+    }
+}
