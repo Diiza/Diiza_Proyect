e3307ba060c822cad5ba2ad1b403cb43a5ab6b39
Kary-Garcia <60681687+Kary-Garcia@users.noreply.github.com>
2020-05-18 00:07:38 -0500

    F
    F

--- Assets/Scripts/Ejercicios.cs
+++ Assets/Scripts/Ejercicios.cs
@@ -14,47 +14,5 @@ public class Ejercicios : MonoBehaviour
 
     public void EjemNumeros()
     {
-        AbrirMenu alf = gameObject.GetComponent<AbrirMenu>();
-        //alf.GetComponent<AbrirMenu>();
-        alfabeto1 = alf.alfabeto;
-        Debug.Log("LEER EL DATO");
-        Debug.Log(alfabeto1);
-
-        AbrirMenu num = new AbrirMenu();
-        num.GetComponent<AbrirMenu>();
-        numeros1 = num.numeros;
-        Debug.Log("LEER EL DATO");
-        Debug.Log(numeros1);
-
-        AbrirMenu plant = GetComponent<AbrirMenu>();
-        plantas1 = plant.plantas;
-        Debug.Log(plant.plantas);
-
-        AbrirMenu anim = GetComponent<AbrirMenu>();
-        animales1 = anim.animales;
-
-        AbrirMenu hog = GetComponent<AbrirMenu>();
-        hogar1 = hog.hogar;
-
-        AbrirMenu emoc = GetComponent<AbrirMenu>();
-        emociones1 = emoc.emociones;
-
-        AbrirMenu fam = GetComponent<AbrirMenu>();
-        familia1 = fam.familia;
-
-        AbrirMenu cuerp = GetComponent<AbrirMenu>();
-        cuerpo1 = cuerp.cuerpo;
-
-        AbrirMenu rop = GetComponent<AbrirMenu>();
-        ropa1 = rop.ropa;
-
-        AbrirMenu orac = GetComponent<AbrirMenu>();
-        oraciones1 = orac.oraciones;
-
-        if (numeros1==1)
-        {
-            Menú.SetActive(false);
-            Numeros.SetActive(true);
-        }
     }
 }
