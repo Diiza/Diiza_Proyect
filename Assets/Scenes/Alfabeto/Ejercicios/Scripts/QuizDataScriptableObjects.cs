cb7f382b44a402190a1cd1dd901cf652bf7f4cf2
Juan Mora <60519779+JuanMora-spg@users.noreply.github.com>
2020-09-30 12:02:52 -0500

    Ejercicio de Palabras (Vocales y Consonantes)

---
+++ Assets/Scenes/Alfabeto/Ejercicios/Scripts/QuizDataScriptableObjects.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections;
+
+[CreateAssetMenu(fileName = "QuestionD", menuName = "QuestionD", order = 1)]
+
+public class QuizDataScriptableObjects : ScriptableObject
+{
+    public List<QuestionDatas> questions;
+    public List<OptionDatas> options;
+
+}
---
+++ Assets/Scenes/Alfabeto/Ejercicios/Scripts/Results.cs
@@ -0,0 +1,29 @@
+﻿
+public class Results
+{
+    private string textName;
+    private string resultQ;
+    
+
+    public Results(string textName, bool isCorrect){
+        this.textName=textName;
+        statusAnswer(isCorrect);
+    }
+
+    private void  statusAnswer(bool isCorrect){
+        if(isCorrect==true){
+            resultQ="✓";
+        }else{
+            resultQ="x";
+        }
+    }
+
+    public string getName(){
+        return textName;
+    }    
+
+    public string getResult(){
+        return resultQ;
+    }
+    
+}
