0bb4aec7d303af06782498b263cee1cfd832a080
Juan Mora <60519779+JuanMora-spg@users.noreply.github.com>
2020-10-13 00:20:18 -0500

    Vocales_ReglasGramaticales_Completadas

---
+++ Assets/Scenes/Alfabeto/Vocales/Scripts/BtnEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class BtnEvent : MonoBehaviour
+{
+    private Button btn;
+
+    void Start()
+    {
+        btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(ExecuteEvent);
+    }
+
+    private void ExecuteEvent()
+    {
+        ExamplesManager.instance.Com(btn);
+    }
+}
