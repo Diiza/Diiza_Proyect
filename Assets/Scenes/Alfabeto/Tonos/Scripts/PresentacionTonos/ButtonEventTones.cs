78b34684ae3b177419fc44ec9829e286efe123f8
Juan Mora <60519779+JuanMora-spg@users.noreply.github.com>
2020-10-20 17:17:00 -0500

    Tonos_ReglasGramaticales_Completadas

---
+++ Assets/Scenes/Alfabeto/Tonos/Scripts/PresentacionTonos/ButtonEventTones.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEventTones : MonoBehaviour
+{
+    private Button btn;
+
+    void Start()
+    {
+        btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(ExecuteEvent);
+    }
+
+    private void ExecuteEvent()
+    {
+        ButtonsManagerTones.instance.Com(btn);
+    }
+
+    
+}
---
+++ Assets/Scenes/Alfabeto/Tonos/Scripts/PresentacionTonos/ScriptableTones.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName="ScriptableTones" , menuName= "ScriptableTones", order=1)]
+public class ScriptableTones : ScriptableObject
+{
+    public List<DataTones> listData;
+}
