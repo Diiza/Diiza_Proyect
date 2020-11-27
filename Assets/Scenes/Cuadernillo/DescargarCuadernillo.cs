8d2bced705f112d02bc42ecaf9590b7ef91f3064
Juan Mora <60519779+JuanMora-spg@users.noreply.github.com>
2020-11-27 00:08:04 -0600

    Audios y Enlace para descargar Cuadernillo

---
+++ Assets/Scenes/Cuadernillo/DescargarCuadernillo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class DescargarCuadernillo : MonoBehaviour
+{
+    [SerializeField]
+    private Button botonDescargar;
+    [SerializeField]
+    private string enlace;
+
+    void Start()
+    {
+        botonDescargar.onClick.AddListener(Click);
+    }
+
+    private void Click()
+    {
+        Application.OpenURL(enlace);
+    }
+
+}
