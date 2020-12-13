1fc09d7a0a8b96b72d907ff14f01d0ca1352ce5a
Juan Mora <60519779+JuanMora-spg@users.noreply.github.com>
2020-12-13 03:29:13 -0600

    Rediseño de Actividad Completa Palabras y Ajustes Generales

---
+++ Assets/Scenes/PortadaScript/Salir.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Salir : MonoBehaviour
+{
+    [SerializeField]
+    private Button btnSalir;
+
+    void Start()
+    {
+        btnSalir.onClick.AddListener(SalirApp);
+    }
+
+    private void SalirApp()
+    {
+        Application.Quit();
+    }
+}
