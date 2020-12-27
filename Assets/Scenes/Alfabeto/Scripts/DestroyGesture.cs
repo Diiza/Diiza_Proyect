6208fcde7be90b77476233b0deb947a7ec026376
Juan Mora <60519779+JuanMora-spg@users.noreply.github.com>
2020-12-27 00:26:55 -0600

    Adición de enlace de cuadernillo en PDF, boton de informacion

---
+++ Assets/Scenes/Alfabeto/Scripts/DestroyGesture.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class DestroyGesture : MonoBehaviour
+{
+    [SerializeField]
+    private Button btn;
+
+    void Start()
+    {
+        btn.onClick.AddListener(DestroyIcon);
+    }
+
+    private void DestroyIcon()
+    {
+         Destroy(gameObject);
+    }
+}
